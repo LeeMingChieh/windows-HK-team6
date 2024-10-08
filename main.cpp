ff0e1f770574c41f8e7d0a87293e9718d2059f07
WattStar18597 <abc900806123@gmail.com>
2024-10-08 14:35:58 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <QApplication>
+#include <QTabWidget>
+#include <QLabel>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QColorDialog>
+#include <QFontDialog>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QLabel *memberLabel0 = new QLabel("隊長(李銘杰ID:41243118)\n組員1(潘怡潔ID:41243107)\n組員2(雷務馨ID:40643149)\n組員3(吳宗恆ID:50915130)");
+    QTabWidget *tabWidget = new QTabWidget;
+    QWidget *memberTab0 = new QWidget;
+    memberLabel0->setStyleSheet("color: yellow;");
+    QVBoxLayout *memberLayout0 = new QVBoxLayout;
+    memberLayout0->addWidget(memberLabel0);
+    memberTab0->setLayout(memberLayout0);
+
+    QWidget *memberTab2 = new QWidget;
+    QLabel *memberLabel2 = new QLabel("組員2=>雷務馨ID:40643149");
+    QPushButton *styleButton = new QPushButton("Front select");
+    QVBoxLayout *member2Layout = new QVBoxLayout;
+    member2Layout->addWidget(memberLabel2);
+    member2Layout->addWidget(styleButton);
+    memberTab2->setLayout(member2Layout);
+
+    QObject::connect(styleButton, &QPushButton::clicked, [=]() {
+        bool ok;
+        QFont font = QFontDialog::getFont(&ok, memberLabel0->font(), nullptr, "選擇字體");
+        if (ok) {
+            memberLabel0->setFont(font);
+        }
+    });
+
+    tabWidget->addTab(memberTab0, "隊長");
+    tabWidget->addTab(memberTab1, "組員1");
+    tabWidget->addTab(memberTab2, "組員2");
+    tabWidget->addTab(memberTab3, "組員3");
+
+    tabWidget->setWindowTitle("WorkGroup");
+    tabWidget->resize(400, 300);
+    tabWidget->show();
+
+    return app.exec();
+}
